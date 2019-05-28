18e9bcb748b44979e34ce2229314c127d64e2d69
canpool <canpool@163.com>
2019-05-29 04:29:48 +0800

    adjust the switch format （fancybar.cpp)
    (cherry picked from commit b82ca41c82425b6a49221f3348540b1a0c8fab5f)
    Signed-off-by: canpool <canpool@163.com>

--- src/libs/qcanpool/fancybar.cpp
+++ src/libs/qcanpool/fancybar.cpp
@@ -677,28 +677,23 @@ void FancyBarPrivate::handleWidgetMouseEvent(QObject *obj, QEvent *event)
     Q_UNUSED(obj);
 
     switch (event->type()) {
-    default:
-        break;
-
-    case QEvent::MouseButtonPress:
-        handleMousePressEvent(static_cast<QMouseEvent *>(event));
-        break;
-
-    case QEvent::MouseButtonRelease:
-        handleMouseReleaseEvent(static_cast<QMouseEvent *>(event));
-        break;
-
-    case QEvent::MouseMove:
-        handleMouseMoveEvent(static_cast<QMouseEvent *>(event));
-        break;
-
-    case QEvent::Leave:
-        handleLeaveEvent(static_cast<QMouseEvent *>(event));
-        break;
-
-    case QEvent::HoverMove:
-        handleHoverMoveEvent(static_cast<QHoverEvent *>(event));
-        break;
+        case QEvent::MouseButtonPress:
+            handleMousePressEvent(static_cast<QMouseEvent *>(event));
+            break;
+        case QEvent::MouseButtonRelease:
+            handleMouseReleaseEvent(static_cast<QMouseEvent *>(event));
+            break;
+        case QEvent::MouseMove:
+            handleMouseMoveEvent(static_cast<QMouseEvent *>(event));
+            break;
+        case QEvent::Leave:
+            handleLeaveEvent(static_cast<QMouseEvent *>(event));
+            break;
+        case QEvent::HoverMove:
+            handleHoverMoveEvent(static_cast<QHoverEvent *>(event));
+            break;
+        default:
+            break;
     }
 }
 
@@ -1228,41 +1223,36 @@ void FancyBar::setFancyStyle(FancyBar::FancyStyle style)
 bool FancyBar::eventFilter(QObject *object, QEvent *event)
 {
     switch (event->type()) {
-    case QEvent::WindowTitleChange: {
-        if (d->windowTitleChange(object)) {
+        case QEvent::WindowTitleChange: {
+            if (d->windowTitleChange(object)) {
+                return true;
+            }
+            break;
+        }
+        case QEvent::WindowIconChange: {
+            if (d->windowIconChange(object)) {
+                return true;
+            }
+            break;
+        }
+        case QEvent::WindowStateChange: {
+            d->windowStateChange(object);
             return true;
         }
-    }
-    break;
-
-    case QEvent::WindowIconChange: {
-        if (d->windowIconChange(object)) {
+        case QEvent::Resize: {
+            d->windowSizeChange(object);
             return true;
         }
-    }
-    break;
-
-    case QEvent::WindowStateChange: {
-        d->windowStateChange(object);
-        return true;
-    }/*break*/;
-
-    case QEvent::Resize: {
-        d->windowSizeChange(object);
-        return true;
-    }/*break*/;
-
-    case QEvent::MouseMove:
-    case QEvent::HoverMove:
-    case QEvent::MouseButtonPress:
-    case QEvent::MouseButtonRelease:
-    case QEvent::MouseButtonDblClick: {
-        d->handleWidgetMouseEvent(object, event);
-        return true;
-    }/*break*/;
-
-    default:
-        break;
+        case QEvent::MouseMove:
+        case QEvent::HoverMove:
+        case QEvent::MouseButtonPress:
+        case QEvent::MouseButtonRelease:
+        case QEvent::MouseButtonDblClick: {
+            d->handleWidgetMouseEvent(object, event);
+            return true;
+        }
+        default:
+            break;
     }
 
     return QObject::eventFilter(object, event);
