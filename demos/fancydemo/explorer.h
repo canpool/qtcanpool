8177e8fe04161819162b58f1f7c31243b80f5663
canpool <canpool@163.com>
2020-11-08 19:05:25 +0800

    clean explorer
    (cherry picked from commit 640307b87854df0d031846701e1886e5a198e519)
    Signed-off-by: canpool <canpool@163.com>

--- demos/fancydemo/explorer.h
+++ demos/fancydemo/explorer.h
@@ -16,28 +16,10 @@ public:
 
     static QString applicationDirPath();
 
-    static QString getProjectFile();
-    static QString getTestFile();
-    static QString getWarnFile();
-    static QString getScdFile();
-
-    static void setProjectFile(const QString &fileName);
-    static void setTestFile(const QString &fileName);
-    static void setWarnFile(const QString &fileName);
-    static void setScdFile(const QString &fileName);
-
-    static QFileInfoList getCidFiles();
-    static QFileInfoList getDcdFiles();
-    static QFileInfoList getScdFiles();
-    static QFileInfoList getWctFiles();
-
     static QString defaultProjectPath();
-    static QString getCidPath();
 
 //    static QString configFileName();
 
-    static QString getSchemaPath();
-    static QString getScdserver();
     static QFileInfoList getThemeFiles();
 
     static QString getTemplateDir();
