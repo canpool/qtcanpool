canpool/qtcanpool

commit 57e306dde2c573a28acdcd565e4f61f90f83d4e4

qcanpool: add fancy basic class
Add FancyTitleBar, FancyBar, FancyWindow, FancyDialog basic class.
Also, provide a fancy example demos/fancydemo.

Users can customize their own frameless UI based on FancyTitleBar.

Signed-off-by: canpool <canpool@163.com>

src/libs/qcanpool/fancybar.cpp
@@ -1,0 +1,340 @@
+/**
+ * Copyright (C) 2018-2022 maminjie <canpool@163.com>
+ * SPDX-License-Identifier: MulanPSL-2.0
+**/
+#include "fancybar_p.h"
+#include "fancybar.h"
+#include "fancytitlebar.h"
+
+#include "quickaccessbar.h"
+
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QToolButton>
+#include <QMouseEvent>
+#include <QEvent>
+#include <QApplication>
+#include <QStyle>
+
+QCANPOOL_BEGIN_NAMESPACE
+
+#define TITLE_BAR_HEIGHT    25
+#define MENU_BAR_HEIGHT     25
+
+
+FancyBarPrivate::FancyBarPrivate(QWidget *parent)
+    : QWidget(parent)
+    , m_menuBar(nullptr)
+    , m_style(FancyBar::WindowStyle)
+    , m_titleBarHeight(TITLE_BAR_HEIGHT)
+    , m_bQuickAccessVisible(false)
+    , m_bMenuBarVisible(false)
+    , m_bMenuWidgetVisible(false)
+{
+}
+
+FancyBarPrivate::~FancyBarPrivate()
+{}
+
+
+void FancyBarPrivate::init()
+{
+    m_logoLayout = new QHBoxLayout();
+    m_logoLayout->setContentsMargins(0, 0, 0, 0);
+    m_logoLayout->setSpacing(0);
+
+    createTitleWidget();
+    createMenuWidget();
+
+    QVBoxLayout *rightLayout = new QVBoxLayout();
+    rightLayout->setMargin(0);
+    rightLayout->setSpacing(0);
+    rightLayout->addWidget(m_titleWidget);
+    rightLayout->addWidget(m_menuWidget);
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->setContentsMargins(1, 1, 1, 0);
+    layout->setSpacing(0);
+    layout->addLayout(m_logoLayout);
+    layout->addLayout(rightLayout);
+    setLayout(layout);
+
+    QVBoxLayout *qlayout = new QVBoxLayout();
+    qlayout->setMargin(0);
+    qlayout->setSpacing(0);
+    qlayout->addWidget(this);
+    q->setLayout(qlayout);
+}
+
+void FancyBarPrivate::createTitleWidget()
+{
+    m_titleWidget = new QWidget();
+    m_titleWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+
+    m_leftSpacerItem = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Minimum);
+    m_rightSpacerItem = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+    m_quickAccessBar = new QuickAccessBar();
+    m_quickAccessBar->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    m_quickAccessBar->hide();
+
+    m_logoButton = m_titleBar->logoButton();
+    m_titleLabel = m_titleBar->titleLabel();
+    m_systemToolBar = m_titleBar->systemToolBar();
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->setContentsMargins(1, 0, 0, 0);
+    layout->setSpacing(0);
+    layout->addWidget(m_logoButton);
+    layout->addWidget(m_quickAccessBar);
+    layout->addItem(m_leftSpacerItem);
+    layout->addWidget(m_titleLabel);
+    layout->addItem(m_rightSpacerItem);
+    layout->addWidget(m_systemToolBar);
+    m_titleWidget->setLayout(layout);
+}
+
+void FancyBarPrivate::createMenuWidget()
+{
+    m_menuWidget = new QWidget();
+    m_menuWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    m_menuWidget->hide();
+
+    m_menuBarArea = new QHBoxLayout();
+    m_menuBarArea->setMargin(0);
+    m_menuBarArea->setSpacing(0);
+
+    m_middleSpacerItem = new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+    const int sz = qApp->style()->pixelMetric(QStyle::PM_SmallIconSize, Q_NULLPTR, Q_NULLPTR);
+    m_toolBar = new QToolBar();
+    m_toolBar->setObjectName(QLatin1String("qtc_menu_toolbar"));
+    m_toolBar->setIconSize(QSize(sz, sz));
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->setContentsMargins(1, 0, 0, 0);
+    layout->setSpacing(0);
+    layout->addLayout(m_menuBarArea);
+    layout->addItem(m_middleSpacerItem);
+    layout->addWidget(m_toolBar);
+    m_menuWidget->setLayout(layout);
+}
+
+void FancyBarPrivate::showQuickAccessBar(bool show)
+{
+    if (m_bQuickAccessVisible == show) {
+        return;
+    }
+    m_bQuickAccessVisible = show;
+    if (show) {
+        m_quickAccessBar->show();
+        m_leftSpacerItem->changeSize(0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    } else {
+        m_quickAccessBar->hide();
+        m_leftSpacerItem->changeSize(0, 0, QSizePolicy::Minimum, QSizePolicy::Minimum);
+    }
+}
+
+void FancyBarPrivate::showMenuBar(bool show)
+{
+    if (m_bMenuBarVisible == show) {
+        return;
+    }
+    m_bMenuBarVisible = show;
+    if (m_menuBar) {
+        m_menuBar->setVisible(show);
+    }
+
+    if (m_style == FancyBar::WindowStyle) {
+        showMenuWidget(show);
+    }
+}
+
+void FancyBarPrivate::showMenuWidget(bool show)
+{
+    if (m_bMenuWidgetVisible == show) {
+        return;
+    }
+    m_bMenuWidgetVisible = show;
+    m_menuWidget->setVisible(show);
+}
+
+void FancyBarPrivate::setFancyStyle(int style)
+{
+    if (m_style == style) {
+        return;
+    }
+    m_style = style;
+    // clear layout
+    QHBoxLayout *titleLayout = qobject_cast<QHBoxLayout *>(m_titleWidget->layout());
+    titleLayout->removeWidget(m_logoButton);
+    titleLayout->removeWidget(m_quickAccessBar);
+    titleLayout->removeItem(m_menuBarArea);
+    titleLayout->removeItem(m_leftSpacerItem);
+    titleLayout->removeWidget(m_titleLabel);
+    titleLayout->removeItem(m_rightSpacerItem);
+    titleLayout->removeWidget(m_toolBar);
+    titleLayout->removeWidget(m_systemToolBar);
+    QHBoxLayout *menuLayout = qobject_cast<QHBoxLayout *>(m_menuWidget->layout());
+    menuLayout->removeItem(m_menuBarArea);
+    menuLayout->removeItem(m_middleSpacerItem);
+    menuLayout->removeWidget(m_toolBar);
+    m_logoButton->setVisible(false);
+    showMenuWidget(true);
+
+    // set style
+    if (style == FancyBar::DialogStyle) {
+        m_logoButton->setVisible(true);
+        titleLayout->addWidget(m_logoButton, 0, Qt::AlignVCenter);
+        titleLayout->addWidget(m_titleLabel);
+        titleLayout->addItem(m_rightSpacerItem);
+        titleLayout->addWidget(m_systemToolBar);
+        showMenuWidget(false);
+    } else if (style == FancyBar::MergedStyle) {
+        m_logoButton->setVisible(true);
+        titleLayout->addWidget(m_logoButton, 0, Qt::AlignVCenter);
+        titleLayout->addWidget(m_quickAccessBar);
+        // FIXME: menuBar doesn't automatically resize immediately after changing quickAccessBar size
+        titleLayout->addLayout(m_menuBarArea);
+        titleLayout->addItem(m_leftSpacerItem);
+        titleLayout->addWidget(m_titleLabel);
+        titleLayout->addItem(m_rightSpacerItem);
+        titleLayout->addWidget(m_toolBar);
+        titleLayout->addWidget(m_systemToolBar);
+        showMenuWidget(false);
+    } else {
+        // default is FancyBar::WindowStyle
+        m_style = FancyBar::WindowStyle;
+        m_logoButton->setVisible(true);
+        titleLayout->addWidget(m_logoButton, 0, Qt::AlignVCenter);
+        titleLayout->addWidget(m_quickAccessBar);
+        titleLayout->addItem(m_leftSpacerItem);
+        titleLayout->addWidget(m_titleLabel);
+        titleLayout->addItem(m_rightSpacerItem);
+        titleLayout->addWidget(m_systemToolBar);
+        menuLayout->addLayout(m_menuBarArea);
+        menuLayout->addItem(m_middleSpacerItem);
+        menuLayout->addWidget(m_toolBar);
+    }
+}
+
+void FancyBarPrivate::registerWidget(QWidget *widget)
+{
+    m_titleBar = new FancyTitleBar(widget);
+    m_titleBar->setTitleWidget(q);
+
+    connect(m_titleBar, SIGNAL(windowResizable(bool)), q, SIGNAL(maximizationChanged(bool)));
+}
+
+QSize FancyBarPrivate::sizeHint() const
+{
+    int ht = 0;
+    int hm = 0;
+    if (m_menuBar && m_bMenuBarVisible) {
+        hm = qMax(m_menuBar->sizeHint().height(), MENU_BAR_HEIGHT);
+        if (m_bMenuWidgetVisible) {
+            m_menuWidget->setFixedHeight(hm);
+            ht = m_titleBarHeight;
+        } else {
+            ht = qMax(hm, m_titleBarHeight);
+            hm = 0;
+        }
+    } else {
+        ht = m_titleBarHeight;
+    }
+    m_titleWidget->setFixedHeight(ht);
+
+    return QSize(width(), ht + hm);
+}
+
+FancyBar::FancyBar(QWidget *parent)
+    : QWidget(parent), d(new FancyBarPrivate(this))
+{
+    Q_ASSERT(parent);
+    setAttribute(Qt::WA_StyledBackground, true);
+    setObjectName(QLatin1String("qtc_titlebar"));
+
+    d->q = this;
+    d->registerWidget(parent);
+    d->init();
+}
+
+FancyBar::~FancyBar()
+{
+}
+
+FancyTitleBar *FancyBar::titleBar() const
+{
+    return d->m_titleBar;
+}
+
+QMenuBar *FancyBar::menuBar() const
+{
+    if (d->m_menuBar == nullptr) {
+        d->m_menuBar = new QMenuBar();
+        d->m_menuBar->setNativeMenuBar(false);
+        d->m_menuBar->setVisible(true);
+        d->m_menuBar->setAttribute(Qt::WA_Mapped);
+        d->m_menuBar->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+        d->m_menuBarArea->addWidget(d->m_menuBar);
+    }
+
+    return d->m_menuBar;
+}
+
+void FancyBar::showMenuBar(bool show)
+{
+    d->showMenuBar(show);
+}
+
+bool FancyBar::isMenuBarVisible() const
+{
+    return d->m_bMenuBarVisible;
+}
+
+QToolBar *FancyBar::toolBar() const
+{
+    return d->m_toolBar;
+}
+
+QuickAccessBar *FancyBar::quickAccessBar() const
+{
+    return d->m_quickAccessBar;
+}
+
+void FancyBar::showQuickAccess(bool show)
+{
+    d->showQuickAccessBar(show);
+}
+
+bool FancyBar::isQuickAccessVisible() const
+{
+    return d->m_bQuickAccessVisible;
+}
+
+void FancyBar::setWidgetResizable(bool resizable)
+{
+    d->m_titleBar->setWidgetResizable(resizable);
+}
+
+void FancyBar::setTitleBarHeight(int height)
+{
+    if (height > TITLE_BAR_HEIGHT && height <= TITLE_BAR_HEIGHT + 15) {
+        d->m_titleBarHeight = height;
+    }
+}
+
+void FancyBar::setFancyStyle(FancyBar::FancyStyle style)
+{
+    d->setFancyStyle(style);
+}
+
+void FancyBar::updateWidgetFlags()
+{
+    d->m_titleBar->updateWidgetFlags();
+}
+
+QCANPOOL_END_NAMESPACE

demos/demos.pro
@@ -3,3 +3,4 @@ CONFIG += ordered
 
 SUBDIRS = \
     helloworld \
+    fancydemo

demos/fancydemo/fancydemo.pro
@@ -1,0 +1,32 @@
+include(../../qtproject.pri)
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+
+TEMPLATE = app
+TARGET = fancydemo
+DESTDIR = $$IDE_APP_PATH
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+include($$QTCANPOOL_DIR/src/rpath.pri)
+
+# lib
+LIBS *= -l$$qtLibraryName(qcanpool)
+
+SOURCES += \
+    main.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    mainwindow.h
+
+RESOURCES += \
+    fancydemo.qrc
+
+RC_FILE = fancydemo.rc

demos/fancydemo/fancydemo.qrc
@@ -1,0 +1,38 @@
+<RCC>
+    <qresource prefix="/qss">
+        <file alias="classic">res/qss/classic.css</file>
+        <file alias="flatdark">res/qss/flatdark.css</file>
+    </qresource>
+    <qresource prefix="/main">
+        <file alias="logo">res/main/logo.png</file>
+    </qresource>
+    <qresource prefix="/tools">
+        <file alias="new">res/tools/new.png</file>
+        <file alias="open">res/tools/open.png</file>
+        <file alias="save">res/tools/save.png</file>
+        <file alias="save_all">res/tools/save_all.png</file>
+        <file alias="recent">res/tools/recent.png</file>
+        <file alias="copy">res/tools/copy.png</file>
+        <file alias="cut">res/tools/cut.png</file>
+        <file alias="redo">res/tools/redo.png</file>
+        <file alias="undo">res/tools/undo.png</file>
+        <file alias="paste">res/tools/paste.png</file>
+        <file alias="help">res/tools/help.png</file>
+        <file alias="option">res/tools/option.png</file>
+        <file alias="exit">res/tools/exit.png</file>
+        <file alias="start">res/tools/start.png</file>
+        <file alias="suspend">res/tools/suspend.png</file>
+        <file alias="stop">res/tools/stop.png</file>
+        <file alias="menu">res/tools/menu.png</file>
+    </qresource>
+    <qresource prefix="/">
+        <file>res/qtc/close.svg</file>
+        <file>res/qtc/close_dark.svg</file>
+        <file>res/qtc/max.svg</file>
+        <file>res/qtc/max_dark.svg</file>
+        <file>res/qtc/min.svg</file>
+        <file>res/qtc/min_dark.svg</file>
+        <file>res/qtc/restore.svg</file>
+        <file>res/qtc/restore_dark.svg</file>
+    </qresource>
+</RCC>

demos/fancydemo/fancydemo.rc
@@ -1,0 +1,1 @@
+IDI_ICON1 ICON "logo.ico"

demos/fancydemo/main.cpp
@@ -1,0 +1,11 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}

demos/fancydemo/mainwindow.cpp
@@ -1,0 +1,200 @@
+#include "mainwindow.h"
+
+#include <QLabel>
+#include <QIcon>
+#include <QActionGroup>
+#include <QMenuBar>
+#include <QMenu>
+#include <QLineEdit>
+
+#include <QApplication>
+#include <QFile>
+
+#include "qcanpool/fancybar.h"
+#include "qcanpool/fancydialog.h"
+#include "qcanpool/fancytitlebar.h"
+#include "qcanpool/quickaccessbar.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : FancyWindow(parent)
+{
+    setWindowIcon(QIcon(":/main/logo"));
+    setWindowTitle(tr("Qt canpool fancy demo V2"));
+    setMinimumSize(QSize(600, 400));
+    setMouseTracking(true);
+
+    createWindow();
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::createWindow()
+{
+    createQuickAccessBar();
+    createMenuBar();
+    createSystemMenu();
+
+    QLabel *label = new QLabel("I CAN DO IT");
+    label->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+    setCentralWidget(label);
+}
+
+void MainWindow::createQuickAccessBar()
+{
+    if (QuickAccessBar *quickAccessBar = fancyBar()->quickAccessBar())
+    {
+        QAction *action = quickAccessBar->actionCustomizeButton();
+        action->setToolTip(tr("Customize Quick Access Bar"));
+
+        action = quickAccessBar->addAction(QIcon(":/tools/new"), tr("New"));
+        action->setShortcut(QKeySequence::New);
+        action->setToolTip(tr("New File or Project\nCtrl+N"));
+        connect(action, SIGNAL(triggered()), this, SLOT(slotNew()));
+
+        action = quickAccessBar->addAction(QIcon(":/tools/open"), tr("Open"));
+        action->setShortcut(tr("Ctrl+O"));
+        action->setToolTip(tr("Open File or Project\nCtrl+O"));
+        quickAccessBar->setActionVisible(action, false);
+
+        QMenu *menu = new QMenu(this);
+        menu->addAction(tr("action1"));
+        menu->addAction(tr("action2"));
+        menu->addAction(tr("action3"));
+        menu->addAction(tr("action4"));
+        action->setMenu(menu);
+
+        action = quickAccessBar->addAction(QIcon(":/tools/save"), tr("Save"));
+        action->setShortcut(tr("Ctrl+S"));
+        action->setToolTip(tr("Save the active document\nCtrl+S"));
+
+        action = quickAccessBar->addAction(QIcon(":/tools/save_all"), tr("Save All"));
+        action->setShortcut(tr("Ctrl+Shift+S"));
+        action->setToolTip(tr("Save all active documents\nCtrl+Shift+S"));
+
+        action = quickAccessBar->addAction(QIcon(":/tools/undo"), tr("&Undo"));
+        action->setShortcut(QKeySequence::Undo);
+        action->setEnabled(false);
+
+        action = quickAccessBar->addAction(QIcon(":/tools/redo"), tr("&Redo"));
+        action->setShortcut(QKeySequence::Redo);
+        action->setEnabled(false);
+
+        fancyBar()->showQuickAccess(true);
+    }
+}
+
+void MainWindow::createMenuBar()
+{
+    if (QMenuBar *menuBar = fancyBar()->menuBar())
+    {
+        QAction *action = new QAction(tr("action1"), this);
+        QMenu *menu = menuBar->addMenu(tr("&File"));
+        menu->addAction(action);
+        action = new QAction(tr("action1"), this);
+        menu = menuBar->addMenu(tr("&Edit"));
+        menu->addAction(action);
+        action = new QAction(tr("action1"), this);
+        menu = menuBar->addMenu(tr("&Debug"));
+        menu->addAction(action);
+        action = new QAction(tr("action1"), this);
+        menu = menuBar->addMenu(tr("&View"));
+        menu->addAction(action);
+        action = new QAction(tr("action1"), this);
+        menu = menuBar->addMenu(tr("&Window"));
+        menu->addAction(action);
+        action = new QAction(tr("action1"), this);
+        menu = menuBar->addMenu(tr("&Tools"));
+        menu->addAction(action);
+        action = new QAction(tr("action1"), this);
+        menu = menuBar->addMenu(tr("&Help"));
+        menu->addAction(action);
+
+        fancyBar()->showMenuBar(true);
+    }
+
+    QToolBar *toolBar = fancyBar()->toolBar();
+    toolBar->addAction(QIcon(":/tools/new"), tr("new"));
+    toolBar->addAction(QIcon(":/tools/open"), tr("open"));
+    toolBar->addAction(QIcon(":/tools/save"), tr("save"));
+}
+
+void MainWindow::createSystemMenu()
+{
+    QAction *mainAction = new QAction(QIcon(":/tools/menu"), tr("menu"));
+    QMenu *mainMenu = new QMenu(mainAction->text());
+    mainAction->setMenu(mainMenu);
+    fancyBar()->titleBar()->addAction(mainAction);
+
+    QMenu *menu = new QMenu(tr("theme style"));
+    mainMenu->addMenu(menu);
+    QActionGroup *group = new QActionGroup(this);
+    addThemeStyleItem(group, menu->addAction(tr("flatdark")), QString(":/qss/flatdark"));
+    addThemeStyleItem(group, menu->addAction(tr("classic")), QString(":/qss/classic"));
+    emit group->actions().at(0)->trigger();
+
+    menu = new QMenu(tr("window style"));
+    mainMenu->addMenu(menu);
+    group = new QActionGroup(this);
+    addWindowStyleItem(group, menu->addAction(tr("WindowStyle")), FancyBar::WindowStyle);
+    addWindowStyleItem(group, menu->addAction(tr("MergedStyle")), FancyBar::MergedStyle);
+    emit group->actions().at(0)->trigger();
+}
+
+void MainWindow::addThemeStyleItem(QActionGroup *group, QAction *action, const QString &qss)
+{
+    group->addAction(action);
+    action->setCheckable(true);
+    action->setData(QVariant(qss));
+    QObject::connect(action, SIGNAL(triggered()), this, SLOT(slotChangeThemeStyle()));
+}
+
+void MainWindow::addWindowStyleItem(QActionGroup *group, QAction *action, int style)
+{
+    group->addAction(action);
+    action->setCheckable(true);
+    action->setData(QVariant(style));
+    QObject::connect(action, SIGNAL(triggered()), this, SLOT(slotChangeWindowStyle()));
+}
+
+void MainWindow::setThemeStyle(const QString &style)
+{
+    QFile file(style);
+    if (file.open(QFile::ReadOnly)) {
+        QString qss = QLatin1String(file.readAll());
+        qApp->setStyleSheet(qss);
+        file.close();
+    } else {
+        qApp->setStyleSheet(style);
+    }
+}
+
+void MainWindow::slotNew()
+{
+    FancyDialog dialog;
+    dialog.setWindowIcon(QIcon(":/main/logo"));
+    dialog.setWindowTitle(tr("Qt canpool fancy dialog"));
+    dialog.setTitleBarHeight(30);
+    dialog.addAction(QIcon(":/tools/new"), tr("new"));
+    dialog.addAction(QIcon(":/tools/open"), tr("open"));
+    dialog.addAction(QIcon(":/tools/save"), tr("save"));
+    dialog.exec();
+}
+
+void MainWindow::slotChangeThemeStyle()
+{
+    QAction *action = qobject_cast<QAction *>(sender());
+    if (action) {
+        setThemeStyle(action->data().toString());
+    }
+}
+
+void MainWindow::slotChangeWindowStyle()
+{
+    QAction *action = qobject_cast<QAction *>(sender());
+    if (action) {
+        fancyBar()->setFancyStyle(static_cast<FancyBar::FancyStyle>(action->data().toInt()));
+    }
+}
+

commit 9285e53a94b253b4741a246f270f9e43ac70735a

qcanpool: fix menuBar doesn't automatically resize
The menuBar doesn't automatically resize immediately after changing
quickAccessBar size under FancyBar::MergedStyle

Add signal customizeActionChanged in QuickAccessBar. When the signal
is emitted, update the FancyBar.

Fixes: 57e306dde2c573a28acdcd565e4f61f90f83d4e4
       ("qcanpool: add fancy basic class")

Bug: https://gitee.com/icanpool/qtcanpool/issues/I5NACI

Signed-off-by: canpool <canpool@163.com>

src/libs/qcanpool/fancybar.cpp
@@ -80,6 +80,12 @@ void FancyBarPrivate::createTitleWidget()
     m_quickAccessBar = new QuickAccessBar();
     m_quickAccessBar->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
     m_quickAccessBar->hide();
+    // to resize menuBar when quickAccessBar size was changed
+    connect(m_quickAccessBar, &QuickAccessBar::customizeActionChanged, [&] () {
+        if (m_style == FancyBar::MergedStyle && m_bMenuBarVisible && m_menuBar) {
+            q->update();
+        }
+    });
 
     m_logoButton = m_titleBar->logoButton();
     m_titleLabel = m_titleBar->titleLabel();
@@ -197,7 +203,6 @@ void FancyBarPrivate::setFancyStyle(int style)
         m_logoButton->setVisible(true);
         titleLayout->addWidget(m_logoButton, 0, Qt::AlignVCenter);
         titleLayout->addWidget(m_quickAccessBar);
-        // FIXME: menuBar doesn't automatically resize immediately after changing quickAccessBar size
         titleLayout->addLayout(m_menuBarArea);
         titleLayout->addItem(m_leftSpacerItem);
         titleLayout->addWidget(m_titleLabel);
@@ -277,7 +282,6 @@ QMenuBar *FancyBar::menuBar() const
         d->m_menuBar = new QMenuBar();
         d->m_menuBar->setNativeMenuBar(false);
         d->m_menuBar->setVisible(true);
-        d->m_menuBar->setAttribute(Qt::WA_Mapped);
         d->m_menuBar->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
         d->m_menuBarArea->addWidget(d->m_menuBar);
     }

src/libs/qcanpool/quickaccessbar.cpp
@@ -182,6 +182,7 @@ void QuickAccessBar::customizeAction(QAction *action)
     d->m_customizeAction = true;
     if (QuickAccessAction *act = dynamic_cast<QuickAccessAction*>(action)) {
         setActionVisible(act->m_srcAction, !widgetForAction(act->m_srcAction));
+        emit customizeActionChanged();
     }
     d->m_customizeAction = false;
 }

src/libs/qcanpool/quickaccessbar.h
@@ -29,6 +29,7 @@ public:
 
 Q_SIGNALS:
     void showCustomizeMenu(QMenu *menu);
+    void customizeActionChanged();
 
 private Q_SLOTS:
     void customizeAction(QAction *action);