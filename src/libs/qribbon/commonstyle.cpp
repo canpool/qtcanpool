canpool/qtcanpool

commit b544e59c3edf71d8fdc6bdf9efa52c961e64e685

qribbon: clean code warnings
remaining screen related deprecated interfaces to be processed

Signed-off-by: canpool <canpool@163.com>

src/libs/qribbon/commonstyle.cpp
@@ -1116,56 +1116,24 @@ bool CommonStyle::paintAnimation(TypePaint type, int nPrim, const QStyleOption *
             CommonTransition *transition = new CommonTransition;
             transition->setWidget(w);
 
-            QStyleOption *opt = Q_NULL;
-            if (const QStyleOptionComboBox *comboBox = qstyleoption_cast<const QStyleOptionComboBox *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionComboBox));
-                if (opt != Q_NULL)
-                    memcpy(opt, comboBox, sizeof(QStyleOptionComboBox));
-            } else if (const StyleSystemToolButton *btn = qstyleoption_cast<const StyleSystemToolButton *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(StyleSystemToolButton));
-                if (opt != Q_NULL)
-                    memcpy(opt, btn, sizeof(StyleSystemToolButton));
-            } else if (const QStyleOptionToolButton *button =
-                           qstyleoption_cast<const QStyleOptionToolButton *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionToolButton));
-                if (opt != Q_NULL)
-                    memcpy(opt, button, sizeof(QStyleOptionToolButton));
-            } else if (const StyleOptionTitleBar *button = qstyleoption_cast<const StyleOptionTitleBar *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(StyleOptionTitleBar));
-                if (opt != Q_NULL)
-                    memcpy(opt, button, sizeof(StyleOptionTitleBar));
-            } else if (const StyleRibbonOptionHeader *optHendle =
-                           qstyleoption_cast<const StyleRibbonOptionHeader *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(StyleRibbonOptionHeader));
-                if (opt != Q_NULL)
-                    memcpy(opt, optHendle, sizeof(StyleRibbonOptionHeader));
-            } else if (const QStyleOptionButton *btn = qstyleoption_cast<const QStyleOptionButton *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionButton));
-                if (opt != Q_NULL)
-                    memcpy(opt, btn, sizeof(QStyleOptionButton));
-            } else if (const QStyleOptionGroupBox *box = qstyleoption_cast<const QStyleOptionGroupBox *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionGroupBox));
-                if (opt != Q_NULL)
-                    memcpy(opt, box, sizeof(QStyleOptionGroupBox));
-            } else if (const QStyleOptionHeader *pHeader = qstyleoption_cast<const QStyleOptionHeader *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionHeader));
-                if (opt != Q_NULL)
-                    memcpy(opt, pHeader, sizeof(QStyleOptionHeader));
-            } else if (const QStyleOptionComplex *pOptionComplex =
-                           qstyleoption_cast<const QStyleOptionComplex *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionComplex));
-                if (opt != Q_NULL)
-                    memcpy(opt, pOptionComplex, sizeof(QStyleOptionComplex));
-            } else if (const QStyleOptionFrame *pOptionComplex = qstyleoption_cast<const QStyleOptionFrame *>(option)) {
-                opt = (QStyleOption *)malloc(sizeof(QStyleOptionFrame));
-                if (opt != Q_NULL)
-                    memcpy(opt, pOptionComplex, sizeof(QStyleOptionFrame));
-            }
+            bool bTransition = qstyleoption_cast<const QStyleOptionComboBox *>(option) ||
+                    qstyleoption_cast<const StyleSystemToolButton *>(option) ||
+                    qstyleoption_cast<const QStyleOptionToolButton *>(option) ||
+                    qstyleoption_cast<const StyleOptionTitleBar *>(option) ||
+                    qstyleoption_cast<const StyleRibbonOptionHeader *>(option) ||
+                    qstyleoption_cast<const QStyleOptionButton *>(option) ||
+                    qstyleoption_cast<const QStyleOptionGroupBox *>(option) ||
+                    qstyleoption_cast<const QStyleOptionHeader *>(option) ||
+                    qstyleoption_cast<const QStyleOptionComplex *>(option) ||
+                    qstyleoption_cast<const QStyleOptionFrame *>(option);
 
-            if (opt == Q_NULL) {
+            if (!bTransition) {
                 delete transition;
                 return false;
             }
+
+            QStyleOption optTmp = *option;
+            QStyleOption *opt = &optTmp;
 
             opt->rect.setRect(0, 0, rect.width(), rect.height());
 
@@ -1213,8 +1181,6 @@ bool CommonStyle::paintAnimation(TypePaint type, int nPrim, const QStyleOption *
             default:
                 break;
             }
-
-            free(opt);
 
             transition->setEndImage(endImage);
 

src/libs/qribbon/officeframehelper_win.cpp
@@ -450,7 +450,7 @@ void OfficeFrameHelperWin::restoreMargins()
             // Reduce top frame to zero since we paint it ourselves.
             const QMargins customMargins =
                 QMargins(m_oldMarginsleft, m_oldMarginstop, m_oldMarginsright, m_oldMarginsbottom);
-            const QVariant customMarginsV = qVariantFromValue(customMargins);
+            const QVariant customMarginsV = QVariant::fromValue(customMargins);
 
             // The dynamic property takes effect when creating the platform window.
             window->setProperty("_q_windowsCustomMargins", customMarginsV);
@@ -1042,8 +1042,12 @@ void OfficeFrameHelperWin::fillSolidRect(QPainter *painter, const QRect &rect, c
     if (hdc == 0)
         return;
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
     QVector<QRect> rects = airRegion.rects();
     for (QVector<QRect>::iterator it = rects.begin(); it != rects.end(); ++it) {
+#else // since 5.8
+    for (QRegion::const_iterator it = airRegion.begin(); it != airRegion.end(); ++it) {
+#endif
         QRect r = rect.intersected(*it);
         RECT rc;
         rc.left = r.left();
@@ -1417,7 +1421,7 @@ void OfficeFrameHelperWin::collapseTopFrame()
 
             // Reduce top frame to zero since we paint it ourselves.
             const QMargins customMargins = QMargins(-val, -valTitle, -val, -valBottom);
-            const QVariant customMarginsV = qVariantFromValue(customMargins);
+            const QVariant customMarginsV = QVariant::fromValue(customMargins);
 
             QMargins oldcustomMargins =
                 qvariant_cast<QMargins>(QGuiApplication::platformNativeInterface()->windowProperty(

src/libs/qribbon/officepaintmanager.cpp
@@ -446,7 +446,7 @@ bool OfficePaintManager::drawIndicatorCheckRadioButton(QStyle::PrimitiveElement 
             const qreal checkmarkRadius = outlineRadius / 2.32;
             circle.addEllipse(circleCenter, checkmarkRadius, checkmarkRadius);
             QColor checkMarkColor = highlighted && enabled ? m_clrAccentText : m_clrBarDkShadow;
-            checkMarkColor = checkMarkColor.dark(180);
+            checkMarkColor = checkMarkColor.darker(180);
             checkMarkColor.setAlpha(200);
             p->setPen(checkMarkColor);
             checkMarkColor.setAlpha(180);
@@ -473,7 +473,7 @@ bool OfficePaintManager::drawIndicatorCheckRadioButton(QStyle::PrimitiveElement 
         if (stateChecked == 1) {
             p->setRenderHint(QPainter::Antialiasing, true);
             QColor checkMarkColor = highlighted && enabled ? m_clrAccentText : m_clrBarDkShadow;
-            checkMarkColor = checkMarkColor.dark(180);
+            checkMarkColor = checkMarkColor.darker(180);
 
             QPen checkPen = QPen(checkMarkColor, DrawHelpers::dpiScaled(1.1));
             checkMarkColor.setAlpha(210);

src/libs/qribbon/officepopupwindow.cpp
@@ -513,7 +513,7 @@ void OfficePopupWindowPrivate::initTitleBarStyleOption(StyleOptionPopupTitleBar 
     option->text = m_titleTxt;
     QFontMetrics fm(m_titleFont);
     option->fontMetrics = fm;
-    option->subControls = 0;
+    option->subControls = QStyle::SubControls();
 
     if (!m_titleTxt.isEmpty())
         option->subControls = QStyle::SC_TitleBarLabel;

src/libs/qribbon/officestyle.cpp
@@ -1008,7 +1008,11 @@ QSize OfficeStyle::sizeFromContents(ContentsType ct, const QStyleOption *opt, co
                 QFont fontBold = menuitem->font;
                 fontBold.setBold(true);
                 QFontMetrics fmBold(fontBold);
+#if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
                 w += fmBold.width(menuitem->text) + 6 + 6;
+#else
+                w += fmBold.horizontalAdvance(menuitem->text) + 6 + 6;
+#endif
                 int h = menuitem->fontMetrics.height() + 6;
                 s.setHeight(qMax(h, 22));
                 s.setWidth(qMax(w, s.width()));
@@ -1837,13 +1841,13 @@ bool OfficeStyle::drawPanelButtonTool(const QStyleOption *opt, QPainter *p, cons
 {
     Q_UNUSED(w);
     if (const QStyleOptionToolButton *toolbutton = qstyleoption_cast<const QStyleOptionToolButton *>(opt)) {
-        bool smallSize = opt->rect.height() < 33;
+        // bool smallSize = opt->rect.height() < 33;
 
         bool enabled = opt->state & State_Enabled;
         bool checked = opt->state & State_On;
         bool selected = opt->state & State_MouseOver;
-        bool mouseInSplit = opt->state & State_MouseOver && toolbutton->activeSubControls & SC_ToolButton;
-        bool mouseInSplitDropDown = opt->state & State_MouseOver && toolbutton->activeSubControls & SC_ToolButtonMenu;
+        // bool mouseInSplit = opt->state & State_MouseOver && toolbutton->activeSubControls & SC_ToolButton;
+        // bool mouseInSplitDropDown = opt->state & State_MouseOver && toolbutton->activeSubControls & SC_ToolButtonMenu;
         bool pressed = opt->state & State_Sunken;
         bool popuped = (toolbutton->activeSubControls & QStyle::SC_ToolButtonMenu) && (opt->state & State_Sunken);
 
@@ -2003,13 +2007,6 @@ bool OfficeStyle::drawSizeGrip(const QStyleOption *opt, QPainter *p, const QWidg
 {
     QTC_D(const OfficeStyle);
     return d->officePaintManager()->drawSizeGrip(opt, p, w);
-}
-
-static QRect getSourceH(QRect rcSrc, int nState, int nCount)
-{
-    QRect rcImage(0, 0, rcSrc.width() / nCount, rcSrc.height());
-    rcImage.translate(nState * rcImage.width(), 0);
-    return rcImage;
 }
 
 // for Slider

src/libs/qribbon/popuphelpers.cpp
@@ -228,14 +228,14 @@ void PopupDrawHelper::drawPrimitive(PrimitiveElement element, const QStyleOption
         if (const QStyleOptionFrame *optFrame = qstyleoption_cast<const QStyleOptionFrame *>(option)) {
             QRect rect = option->rect;
             int titleBarHeight = proxy()->pixelMetric(PM_TitleBarHeight, 0, 0);
-            p->fillRect(rect, /*optFrame->palette*/ m_palBackground.background().color());
+            p->fillRect(rect, /*optFrame->palette*/ m_palBackground.window().color());
             rect.adjust(3, titleBarHeight, -3, -3);
-            p->fillRect(rect, /*optFrame->palette*/ m_palBackground.foreground().color());
+            p->fillRect(rect, /*optFrame->palette*/ m_palBackground.windowText().color());
             rect.adjust(optFrame->lineWidth, optFrame->lineWidth, -optFrame->lineWidth, -optFrame->lineWidth);
             rect.setBottom(rect.top() + 30);
             DrawHelpers::drawGradientFill(*p, rect,
-                                          /*optFrame->palette*/ m_palBackground.foreground().color().dark(111),
-                                          /*optFrame->palette*/ m_palBackground.foreground().color(), true);
+                                          /*optFrame->palette*/ m_palBackground.windowText().color().darker(111),
+                                          /*optFrame->palette*/ m_palBackground.windowText().color(), true);
             break;
         }
         // fallthrough
@@ -291,8 +291,8 @@ void PopupOffice2000DrawHelper::drawPrimitive(PrimitiveElement element, const QS
     case PE_FrameWindow:
         if (const QStyleOptionFrame *optFrame = qstyleoption_cast<const QStyleOptionFrame *>(option)) {
             QRect rect = optFrame->rect;
-            DrawHelpers::drawGradientFill(*p, rect, m_palBackground.background().color(),
-                                          m_palBackground.background().color(), true);
+            DrawHelpers::drawGradientFill(*p, rect, m_palBackground.window().color(),
+                                          m_palBackground.window().color(), true);
             DrawHelpers::draw3DRect(*p, m_palFrame.shadow().color(), m_palFrame.shadow().color(), rect.x(), rect.y(),
                                     rect.width() - 1, rect.height() - 1, false);
 

src/libs/qribbon/ribbonbar.cpp
@@ -442,8 +442,13 @@ void RibbonBarPrivate::layoutRibbon()
         if (validIndex(index)) {
             if (RibbonPage *page = m_listPages.at(index)) {
                 QRect rectLogotype;
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
                 int width = m_logotypeLabel->pixmap()->width();
                 int height = m_logotypeLabel->pixmap()->height();
+#else
+                int width = m_logotypeLabel->pixmap(Qt::ReturnByValue).width();
+                int height = m_logotypeLabel->pixmap(Qt::ReturnByValue).height();
+#endif
                 QRect rcPage = page->geometry();
 
                 if (height < rcPage.height())
@@ -2086,7 +2091,11 @@ QPixmap RibbonBar::logoPixmap(Qt::AlignmentFlag &alignment) const
     QTC_D(const RibbonBar);
 
     alignment = d->m_logotypeLabel->alignmentLogotype();
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
     return d->m_logotypeLabel->isVisible() ? *d->m_logotypeLabel->pixmap() : QPixmap();
+#else
+    return d->m_logotypeLabel->isVisible() ? d->m_logotypeLabel->pixmap(Qt::ReturnByValue) : QPixmap();
+#endif
 }
 
 /*!
@@ -2848,12 +2857,20 @@ void RibbonBar::wheelEvent(QWheelEvent *event)
     if (!isMaximized())
         return;
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
     QPoint pos = event->pos();
+#else
+    QPoint pos = event->position().toPoint();
+#endif
 
     if (!geometry().contains(pos))
         return;
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
     d->m_tabBar->currentNextTab(event->delta() < 0);
+#else
+    d->m_tabBar->currentNextTab(event->angleDelta().y() < 0);
+#endif
 }
 
 void RibbonBar::contextMenuEvent(QContextMenuEvent *event)

src/libs/qribbon/ribbonbutton_p.cpp
@@ -100,7 +100,11 @@ QSize RibbonButton::sizeHint() const
             w += mode == MenuButtonPopup || mode == QToolButton::InstantPopup ? 4 : 10;
         } else {
             QSize textSize = opt.fontMetrics.size(Qt::TextShowMnemonic, textButton);
+#if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
             textSize.setWidth(textSize.width() + opt.fontMetrics.width(QLatin1Char(' ')) * 2);
+#else
+            textSize.setWidth(textSize.width() + opt.fontMetrics.horizontalAdvance(QLatin1Char(' ')) * 2);
+#endif
             h = sz.height();
 
             if (opt.toolButtonStyle == Qt::ToolButtonTextBesideIcon) {

commit 60b7ce2fc374db665d16c8589ce8a5d1e0c16f41

qribbon: fix the animated transition exception
Fixes: b544e59 ("qribbon: clean code warnings")

Signed-off-by: canpool <canpool@163.com>

src/libs/qribbon/commonstyle.cpp
@@ -1132,8 +1132,7 @@ bool CommonStyle::paintAnimation(TypePaint type, int nPrim, const QStyleOption *
                 return false;
             }
 
-            QStyleOption optTmp = *option;
-            QStyleOption *opt = &optTmp;
+            QStyleOption *opt = const_cast<QStyleOption *>(option);
 
             opt->rect.setRect(0, 0, rect.width(), rect.height());
 