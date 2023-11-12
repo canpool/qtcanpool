d957e7d8c853b00312e13bc6a90ed5666cacc6bb
canpool <canpool@163.com>
2023-11-12 14:16:00 +0800

    project: update pch headers
    Signed-off-by: canpool <canpool@163.com>

--- src/shared/qtproject_gui_pch.h
+++ src/shared/qtproject_gui_pch.h
@@ -1,32 +1,18 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
+/**
+ * Copyright (C) 2023 maminjie <canpool@163.com>
+ * Copyright (C) 2016 The Qt Company Ltd.
+ * SPDX-License-Identifier: GPL-3.0
+ **/
 
 /*
  * This is a precompiled header file for use in Xcode / Mac GCC /
  * GCC >= 3.4 / VC to greatly speed the building of Qt Creator.
+ *
+ * The define below is checked in source code. Do not replace with #pragma once!
  */
+
+#ifndef QTPROJECT_GUI_PCH_H
+#define QTPROJECT_GUI_PCH_H
 
 #include "qtproject_pch.h"
 
@@ -37,7 +23,9 @@
 #include <QApplication>
 #include <QBitmap>
 #include <QCursor>
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 #include <QDesktopWidget>
+#endif
 #include <QImage>
 #include <QLayout>
 #include <QPainter>
@@ -46,3 +34,5 @@
 #include <QWidget>
 
 #endif
+
+#endif // QTPROJECT_GUI_PCH_H
--- src/shared/qtproject_pch.h
+++ src/shared/qtproject_pch.h
@@ -1,32 +1,18 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
+/**
+ * Copyright (C) 2023 maminjie <canpool@163.com>
+ * Copyright (C) 2016 The Qt Company Ltd.
+ * SPDX-License-Identifier: GPL-3.0
+ **/
 
 /*
  * This is a precompiled header file for use in Xcode / Mac GCC /
  * GCC >= 3.4 / VC to greatly speed the building of Qt Creator.
+ *
+ * The define below is checked in source code. Do not replace with #pragma once!
  */
+
+#ifndef QTPROJECT_PCH_H
+#define QTPROJECT_PCH_H
 
 #if defined __cplusplus
 #include <QtGlobal>
@@ -52,3 +38,5 @@
 
 #include <stdlib.h>
 #endif
+
+#endif // QTPROJECT_PCH_H
