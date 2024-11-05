bd1451e1d3c99bfe72a5c516df170d79a21cadf6
canpool <canpool@163.com>
2024-11-05 20:06:15 +0800

    qxdock: init DockTabBar
    Signed-off-by: canpool <canpool@163.com>

--- src/libs/qxdock/docktabbar.cpp
+++ src/libs/qxdock/docktabbar.cpp
@@ -5,6 +5,9 @@
 
 #include "docktabbar.h"
 #include "dockpanel.h"
+#include "docktab.h"
+
+#include <QBoxLayout>
 
 QX_DOCK_BEGIN_NAMESPACE
 
@@ -14,13 +17,31 @@ public:
     QX_DECLARE_PUBLIC(DockTabBar)
 public:
     DockTabBarPrivate();
+    void init();
 public:
     DockPanel *m_panel = nullptr;
+    QWidget *m_tabsContainerWidget;
+    QBoxLayout *m_tabsLayout;
 };
 
 DockTabBarPrivate::DockTabBarPrivate()
 {
 
+}
+
+void DockTabBarPrivate::init()
+{
+    Q_Q(DockTabBar);
+
+    m_tabsContainerWidget = new QWidget();
+    m_tabsContainerWidget->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    m_tabsContainerWidget->setObjectName("tabsContainerWidget");
+    m_tabsLayout = new QBoxLayout(QBoxLayout::LeftToRight);
+    m_tabsLayout->setContentsMargins(0, 0, 0, 0);
+    m_tabsLayout->setSpacing(0);
+    m_tabsLayout->addStretch(1);
+    m_tabsContainerWidget->setLayout(m_tabsLayout);
+    q->setWidget(m_tabsContainerWidget);
 }
 
 DockTabBar::DockTabBar(DockPanel *parent)
@@ -29,6 +50,14 @@ DockTabBar::DockTabBar(DockPanel *parent)
     QX_INIT_PRIVATE(DockTabBar)
     Q_D(DockTabBar);
     d->m_panel = parent;
+    d->init();
+
+    setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    setFrameStyle(QFrame::NoFrame);
+    setWidgetResizable(true);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFocusPolicy(Qt::NoFocus);
 }
 
 DockTabBar::~DockTabBar()
