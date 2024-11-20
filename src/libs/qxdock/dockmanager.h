343f8b598f8afb9ff90182c0a632a8ba2be29d59
canpool <canpool@163.com>
2024-11-20 18:05:42 +0800

    qxdock: perfect DockManager
    Signed-off-by: canpool <canpool@163.com>

--- src/libs/qxdock/dockmanager.h
+++ src/libs/qxdock/dockmanager.h
@@ -51,6 +51,7 @@ public:
 
         DefaultBaseConfig = DefaultDockAreaButtons
                           | ActiveTabHasCloseButton
+                          | XmlCompressionEnabled
                           | FloatingContainerHasWidgetTitle,
 
         DefaultOpaqueConfig = DefaultBaseConfig
