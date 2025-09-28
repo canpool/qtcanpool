58b8c191ad650ec13bec876f7e180ea41e34bd95
canpool <canpool@163.com>
2025-09-28 19:47:35 +0800

    ribbondemo: refactor RibbonAppWindow based on RibbonMainWindow
    Signed-off-by: canpool <canpool@163.com>

--- demos/qxwidget/ribbondemo/ribbonappwindow.cpp
+++ demos/qxwidget/ribbondemo/ribbonappwindow.cpp
@@ -1,14 +1,11 @@
 /**
- * Copyleft (C) 2023 maminjie <canpool@163.com>
+ * Copyleft (C) 2023-2025 maminjie <canpool@163.com>
  * SPDX-License-Identifier: MIT
-**/
+ **/
+
 #include "ribbonappwindow.h"
-#include "qxribbon/ribbonbar.h"
 #include "qxribbon/windowbuttongroup.h"
 #include "qxwindow/windowagentwidget.h"
-
-#include <QApplication>
-#include <QFile>
 
 QX_WINDOW_USE_NAMESPACE
 
@@ -19,46 +16,35 @@ class RibbonAppWindowPrivate
     QX_DECLARE_PUBLIC(RibbonAppWindow)
 public:
     RibbonAppWindowPrivate();
-    void setMenuWidget(QWidget *menuBar);
-    void resizeRibbon();
+    void init();
 public:
-    RibbonBar *m_ribbonBar;
     WindowButtonGroup *m_windowButtonGroup;
     WindowAgentWidget *m_windowAgent;
-    int m_theme;
 };
 
 RibbonAppWindowPrivate::RibbonAppWindowPrivate()
-    : m_ribbonBar(Q_NULLPTR)
-    , m_windowButtonGroup(Q_NULLPTR)
+    : m_windowButtonGroup(Q_NULLPTR)
     , m_windowAgent(Q_NULLPTR)
-    , m_theme(RibbonTheme::LightOffice2013Theme)
 {
 }
 
-void RibbonAppWindowPrivate::setMenuWidget(QWidget *menuBar)
+void RibbonAppWindowPrivate::init()
 {
     Q_Q(RibbonAppWindow);
-    RibbonBar *bar = qobject_cast<RibbonBar *>(menuBar);
+    RibbonBar *ribbonBar = q->ribbonBar();
 
-    if (bar) {
-        m_ribbonBar = bar;
-        m_ribbonBar->installEventFilter(q);
-    } else {
-        m_ribbonBar = Q_NULLPTR;
-    }
     if (Q_NULLPTR == m_windowAgent) {
         m_windowAgent = new WindowAgentWidget(q);
         m_windowAgent->setup(q);
-        m_windowAgent->setTitleBar(bar);
+        m_windowAgent->setTitleBar(ribbonBar);
         m_windowAgent->addCaptionClassName(QString::fromLatin1("QxRibbon::RibbonBar"));
     }
 
     if (Q_NULLPTR == m_windowButtonGroup) {
         m_windowButtonGroup = new WindowButtonGroup(q);
         m_windowButtonGroup->setSignalEnabled(true);
-        QObject::connect(m_windowButtonGroup, &WindowButtonGroup::buttonMinimizeClicked,
-                         q, &RibbonAppWindow::showMinimized);
+        QObject::connect(m_windowButtonGroup, &WindowButtonGroup::buttonMinimizeClicked, q,
+                         &RibbonAppWindow::showMinimized);
         QObject::connect(m_windowButtonGroup, &WindowButtonGroup::buttonMaximzieClicked, q, [q]() {
             if (q->isMaximized()) {
                 q->showNormal();
@@ -66,10 +52,9 @@ void RibbonAppWindowPrivate::setMenuWidget(QWidget *menuBar)
                 q->showMaximized();
             }
         });
-        QObject::connect(m_windowButtonGroup, &WindowButtonGroup::buttonCloseClicked,
-                         q, &RibbonAppWindow::close);
-        if (m_ribbonBar) {
-            m_ribbonBar->setWindowButtonGroup(m_windowButtonGroup);
+        QObject::connect(m_windowButtonGroup, &WindowButtonGroup::buttonCloseClicked, q, &RibbonAppWindow::close);
+        if (ribbonBar) {
+            ribbonBar->setWindowButtonGroup(m_windowButtonGroup);
         }
         m_windowAgent->setSystemButton(WindowAgentBase::Maximize, m_windowButtonGroup->widgetForMaximizeButton());
     }
@@ -83,51 +68,18 @@ void RibbonAppWindowPrivate::setMenuWidget(QWidget *menuBar)
     m_windowAgent->setHitTestVisible(m_windowButtonGroup, true);
 }
 
-void RibbonAppWindowPrivate::resizeRibbon()
-{
-    if (m_ribbonBar == Q_NULLPTR) {
-        return;
-    }
-    m_ribbonBar->resizeRibbon();
-}
-
 RibbonAppWindow::RibbonAppWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : RibbonMainWindow(parent)
 {
     QX_INIT_PRIVATE(RibbonAppWindow)
     Q_D(RibbonAppWindow);
-    setRibbonTheme(ribbonTheme());
-    setMenuWidget(new RibbonBar(this));
-    d->resizeRibbon();
+    d->init();
+    resizeRibbon();
 }
 
 RibbonAppWindow::~RibbonAppWindow()
 {
-    QX_FINI_PRIVATE()
-}
-
-RibbonBar *RibbonAppWindow::ribbonBar() const
-{
-    Q_D(const RibbonAppWindow);
-    return d->m_ribbonBar;
-}
-
-void RibbonAppWindow::setRibbonTheme(int theme)
-{
-    Q_D(RibbonAppWindow);
-    RibbonTheme::setTheme(theme, this);
-    d->m_theme = theme;
-
-    if (d->m_ribbonBar) {
-        d->m_ribbonBar->updateRibbonTheme();
-        d->m_ribbonBar->resizeRibbon();
-    }
-}
-
-int RibbonAppWindow::ribbonTheme() const
-{
-    Q_D(const RibbonAppWindow);
-    return d->m_theme;
+    QX_FINI_PRIVATE();
 }
 
 void RibbonAppWindow::updateWindowFlags(Qt::WindowFlags flags)
@@ -138,10 +90,7 @@ void RibbonAppWindow::updateWindowFlags(Qt::WindowFlags flags)
             showNormal();
         }
     }
-    setWindowFlags(flags);
-    // Note: This function setWindowFlags calls setParent() when changing the flags for a window,
-    // causing the widget to be hidden. You must call show() to make the widget visible again..
-    show();
+    RibbonMainWindow::updateWindowFlags(flags);
     if (d->m_windowButtonGroup) {
         if (flags & Qt::WindowMaximizeButtonHint) {
             d->m_windowAgent->setSystemButton(WindowAgentBase::Maximize,
@@ -155,27 +104,8 @@ void RibbonAppWindow::updateWindowFlags(Qt::WindowFlags flags)
         // I tried to hide the button group first and display the button group after resize ribbon,
         // but because resize ribbon is a trigger event and non-blocking, the button group was displayed
         // before resize was completed, so I failed.
-        d->resizeRibbon();
+        resizeRibbon();
     }
-}
-
-void RibbonAppWindow::setMenuWidget(QWidget *menuBar)
-{
-    Q_D(RibbonAppWindow);
-    QMainWindow::setMenuWidget(menuBar);
-    d->setMenuWidget(menuBar);
-}
-
-void RibbonAppWindow::setMenuBar(QMenuBar *menuBar)
-{
-    Q_D(RibbonAppWindow);
-    QMainWindow::setMenuBar(menuBar);
-    d->setMenuWidget(menuBar);
-}
-
-bool RibbonAppWindow::eventFilter(QObject *obj, QEvent *e)
-{
-    return QMainWindow::eventFilter(obj, e);
 }
 
 bool RibbonAppWindow::event(QEvent *e)
@@ -191,26 +121,14 @@ bool RibbonAppWindow::event(QEvent *e)
                 d->m_windowButtonGroup->setVisible(true);
             }
             d->m_windowButtonGroup->setWindowStates(s);
-            d->resizeRibbon();
+            resizeRibbon();
         }
         Q_EMIT windowStateChanged(s);
-    } break;
-    case QEvent::Resize: {
-        if (d->m_ribbonBar) {
-            if (d->m_ribbonBar->size().width() != this->size().width()) {
-                d->m_ribbonBar->setFixedWidth(this->size().width());
-            }
-        }
     } break;
     default:
         break;
     }
-    return QMainWindow::event(e);
-}
-
-void RibbonAppWindow::loadTheme(const QString &themeFile)
-{
-    RibbonTheme::loadTheme(themeFile, this);
+    return RibbonMainWindow::event(e);
 }
 
 QX_RIBBON_END_NAMESPACE
--- demos/qxwidget/ribbondemo/ribbonappwindow.h
+++ demos/qxwidget/ribbondemo/ribbonappwindow.h
@@ -1,41 +1,25 @@
 /**
- * Copyleft (C) 2023 maminjie <canpool@163.com>
+ * Copyleft (C) 2023-2025 maminjie <canpool@163.com>
  * SPDX-License-Identifier: MIT
 **/
 #pragma once
 
-#include "qxribbon/qxribbon_global.h"
-#include "qxribbon/ribbontheme.h"
-#include <QMainWindow>
+#include "qxribbon/ribbonmainwindow.h"
 
 QX_RIBBON_BEGIN_NAMESPACE
 
 class RibbonAppWindowPrivate;
-class RibbonBar;
 
-class RibbonAppWindow : public QMainWindow
+class RibbonAppWindow : public RibbonMainWindow
 {
     Q_OBJECT
-    Q_PROPERTY(int ribbonTheme READ ribbonTheme WRITE setRibbonTheme)
 public:
     RibbonAppWindow(QWidget *parent = Q_NULLPTR);
     ~RibbonAppWindow() Q_DECL_OVERRIDE;
 
-    RibbonBar *ribbonBar() const;
-
-    /** theme, see to RibbonTheme::ThemeStyle */
-    void setRibbonTheme(int theme);
-    int ribbonTheme() const;
-
     void updateWindowFlags(Qt::WindowFlags flags);
 
-    void setMenuWidget(QWidget *menuBar);
-    void setMenuBar(QMenuBar *menuBar);
-Q_SIGNALS:
-    void windowStateChanged(Qt::WindowStates state);
 protected:
-    void loadTheme(const QString &themeFile);
-    virtual bool eventFilter(QObject *obj, QEvent *e) Q_DECL_OVERRIDE;
     virtual bool event(QEvent *e) Q_DECL_OVERRIDE;
 private:
     QX_DECLARE_PRIVATE(RibbonAppWindow)
